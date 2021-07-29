c9e570cbcfc76dc5484697fe99adaf9b6b8cc33f
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-07-29 08:51:32 +0530

    Update status_message_fn.py

--- bot/plugins/status_message_fn.py
+++ bot/plugins/status_message_fn.py
@@ -8,11 +8,11 @@ import time
 import traceback
 
 from pyrogram import Client, filters
-from bot.config import Messages as tr
+from bot.config import Messages as msg
 from bot.dinmamoc import Commandi
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['log']), group=2)
 async def upload_log_file(client, message):
     await message.reply_document = (
-         tr.LOG_FILE_ZZGEVC
+         msg.LOG_FILE_ZZGEVC
     )
