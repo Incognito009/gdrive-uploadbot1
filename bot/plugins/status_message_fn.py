40e86285cc20ff351a34cd5a287b49b562c0c6a1
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-07-29 08:25:26 +0530

    Update status_message_fn.py

--- bot/plugins/status_message_fn.py
+++ bot/plugins/status_message_fn.py
@@ -10,7 +10,7 @@ import traceback
 from bot.config import Messages
 from bot.dinmamoc import Commandi
 
-
+@Client.on_message(filters.private & filters.incoming & filters.command(['log']), group=2)
 async def upload_log_file(client, message):
     await message.reply_document(
         LOG_FILE_ZZGEVC
